5e93da7f4660211129bca7a208f1b656cb97a967
Glu(k)hov Kirill <kirill.gluhov2003@yandex.ru>
2018-12-10 18:16:19 +0300

    Добавлены новые функции в logic.py. Добавлена возможность создавать и удалять напоминалки (таймеры), добавлена возможность загружать и выгружать конфиг цветов различных элементов

--- logic.py
+++ logic.py
@@ -1,3 +1,8 @@
+import math
+
+
+# Блок вычислительных функций
+
 def amount(first, second):
     if (first == "ERR") or (second == "ERR"):
         return "ERR"
@@ -26,14 +31,14 @@ def division(first, second):
         return first / second    
     
 
-def factorial(first):
-    if (first == "ERR") or (first < 0):
+def factorial(number):
+    try:
+        number = int(number)
+        return math.factorial(number)
+    except ValueError:
         return "ERR"
-    else:
-        result = 1
-        for i in range(1, first+1):
-            result *= i
-        return result
+    except TypeError:
+        return "ERR"
     
 
 def raise_to_a_power(number, degree):
@@ -47,3 +52,74 @@ def square_root(number):
     if number < 0:
         return "ERR"
     return number ** 0.5
+
+
+def module(number):
+    return abs(number)
+
+
+def in_decimal_system(number, base):
+    try:
+        return int(str(number), int(base))
+    except ValueError:
+        return "ERR"
+
+
+def pi():
+    return math.pi
+
+
+def e():
+    return math.e
+
+
+def log(number, base=None):
+    try:
+        number = float(number)
+        if base:
+            base = float(base)
+            return math.log(number, base)
+        else:
+            return math.log(number)
+    except ValueError:
+        return "ERR"
+
+
+def degrees_to_radians(degrees):
+    try:
+        degrees = float(degrees)
+        return degrees/360*math.pi*2
+    except ValueError:
+        return False
+
+
+def radians_to_degrees(radians):
+    try:
+        radians = float(radians)
+        degrees = (radians * 180) / pi()
+        return degrees
+    except ValueError:
+        return False
+
+
+def cosine(degrees):   
+    radians = degrees_to_radians(degrees)
+    if radians:
+        return math.cos(radians)
+    return False
+
+
+def sine(degrees):
+    radians = degrees_to_radians(degrees)
+    if radians:
+        return math.sin(radians)
+    return False    
+
+
+def tangent(degrees):
+    radians = degrees_to_radians(degrees)
+    if radians:
+        return math.tan(radians)
+    return False
+
+
--- main.py
+++ main.py
@@ -1,17 +1,60 @@
 import sys
+import json
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.Core import QTimer
 from UI_data import Ui_MainWindow
  
  
 class GreatCalculator(QMainWindow,Ui_MainWindow):
     def __init__(self):
+        self.colors = {}
+        self.notes = {} # <QTimer object> : "note"
         super().__init__()
-        self.initUI()
- 
-    def initUI(self):
-        self.setWindowTitle('Калькулятор с напоминалками')       
- 
- 
+        self.setupUi(self)
+        pass
+    
+    def load_config(self):
+        with open("colors.cnf") as file:
+            try:
+                config = json.loads(file.read())
+                if type(config) != dict:
+                    raise TypeError("Неправильный формат файла")
+                return config
+            except BaseException as err:
+                return err
+        pass
+    
+    def dump_config(self):
+        config = json.dumps(self.colors)
+        with open("colors.cnf", "w") as file:
+            file.write(config)
+        return True
+    
+    def add_timer(self, time, note):
+        # Создание новой заметки
+        # Время передается в секуднах
+        if note in self.notes:
+            raise NameError("Напоминалка с такой заметкой уже существует!")
+        timer = Qtimer(self)
+        timer.singleShot(time*1000, self.ring)
+        self.notes[timer] = note
+    
+    def remove_timer(self, timer):
+        # Удаление заметки
+        try:
+            return self.notes.pop[timer]
+        except KeyError:
+            # TODO: Если такого таймера не существует, то...
+            # TODO: Обсудить реальность такой ситуации
+            pass
+        pass
+    
+    def ring(self):
+        # TODO: Написать функцию, которая будет показывать уведомление, что время истекло (Текст заметки можно взять из remove_timer)
+        pass
+    pass
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = GreatCalculator()
